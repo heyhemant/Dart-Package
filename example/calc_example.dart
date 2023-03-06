82ab50f4e053006f271ca3ba0f2d7b86062bf962
Hemant  Kumar <hemant.kumar@ip-192-168-1-103.ap-south-1.compute.internal>
2023-03-06 19:37:29 +0530

    Dart Package

---
+++ example/calc_example.dart
@@ -0,0 +1,6 @@
+import 'package:calc/calc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/calc.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library calc;
+
+export 'src/calc_base.dart' show Calculations;
+
+// TODO: Export any libraries intended for clients of this package.
