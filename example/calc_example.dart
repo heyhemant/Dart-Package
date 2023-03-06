e34ccb2f87e102ecccdb9674d46956c2f20d449d
Hemant Kumar <hemantbanjara855@gmail.com>
2023-03-06 20:01:49 +0530

    package

--- example/calc_example.dart
+++ example/calc_example.dart
@@ -1,6 +1,6 @@
-import 'package:calc/calc.dart';
+// import 'package:calc/calc.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+// void main() {
+//   var awesome = Awesome();
+//   print('awesome: ${awesome.isAwesome}');
+// }
--- test/calc_test.dart
+++ test/calc_test.dart
@@ -2,15 +2,15 @@ import 'package:calc/calc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  // group('A group of tests', () {
+  //   final awesome = Awesome();
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+  //   setUp(() {
+  //     // Additional setup goes here.
+  //   });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
+  //   test('First Test', () {
+  //     expect(awesome.isAwesome, isTrue);
+  //   });
+  // });
 }
